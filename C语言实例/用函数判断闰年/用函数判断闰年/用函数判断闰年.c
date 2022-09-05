6c3b06af68d58e7221d0c29d2498cda4854994fc
stwings <571641990@qq.com>
2022-09-05 19:25:00 +0800

    用函数判断闰年

---
+++ C语言实例/用函数判断闰年/用函数判断闰年/用函数判断闰年.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int is_leap_year(int x)
+{
+	if (x % 4 == 0 && x % 100 != 0)
+		return 1;
+	else if (x % 400 == 0)
+		return 1;
+	else
+		return 0;
+}
+int main()
+{
+	int y = 0;
+	scanf("%d", &y);
+	if (is_leap_year(y) == 1)
+	{
+		printf("是闰年\n");
+	}
+	else
+	{
+		printf("不是闰年\n");
+	}
+	return 0;
+}
