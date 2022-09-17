9911dc379515313123fc3e16a0bccf34cea332d3
stwings <571641990@qq.com>
2022-09-17 17:23:27 +0800

    扫雷

--- C语言实例/扫雷/扫雷/game.h
+++ C语言实例/扫雷/扫雷/game.h
@@ -1,28 +1,43 @@
 #pragma once
 #define _CRT_SECURE_NO_WARNINGS
+//
+//#define ROW 9
+//#define COL 9
+//
+//#define EASY_COUNT 10
+//
+//#define ROWS ROW+2
+//#define COLS COL+2
+//
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<time.h>
+//
+////≥ı ºªØ∆Â≈Ã
+//void InitBoard(char board[ROWS][COLS], int rows, int cols, char set);
+//
+////¥Ú”°∆Â≈Ã
+//void DisplayBoard(char board[ROWS][COLS], int row, int col);
+//
+////…Ë÷√¿◊
+//void SetMine(char board[ROWS][COLS], int row, int col);
+//
+////≈≈≤È¿◊
+//void FindMine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col);
 
-#define ROW 9
-#define COL 9
-#define EASY_COUNT 10
-
-#define ROWS ROW+2
-#define COLS COL+2
 
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
 
-//≥ı ºªØ∆Â≈Ã
-void InitBoard(char board[ROWS][COLS], int rows, int cols, char set);
+#define ROW 9
+#define COL 9
+#define ROWS ROW + 2
+#define COLS COL + 2
+#define COUNT 10
 
-//¥Ú”°∆Â≈Ã
-void DisplayBoard(char board[ROWS][COLS], int row, int col);
-
-//…Ë÷√¿◊
-void SetMine(char board[ROWS][COLS], int row, int col);
-
-//≈≈≤È¿◊
-void FindMine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col);
-
-
+void init_board(char board[ROWS][COLS], int rows, int cols, char set);
+void set_mine(char mine[ROWS][COLS], int row, int col);
+void display_board(char board[ROWS][COLS], int row, int col);
+void find_mind(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col);
 
--- C语言实例/扫雷/扫雷/扫雷.c
+++ C语言实例/扫雷/扫雷/扫雷.c
@@ -1,56 +1,110 @@
+//#include"game.h"
+//
+//void menu()
+//{
+//	printf("1.play\n");
+//	printf("0.exit\n");
+//}
+//
+//void game()
+//{
+//	char mine[ROWS][COLS] = { 0 };//存放布置好的雷的信息
+//	char show[ROWS][COLS] = { 0 };//存放排查出的雷的信息
+//	
+//	//初始化棋盘
+//	InitBoard(mine, ROWS, COLS, '0');
+//	InitBoard(show, ROWS, COLS, '*');
+//	
+//	//打印棋盘
+//	DisplayBoard(show, ROW, COL);
+//	
+//	//布置雷
+//	SetMine(mine, ROW, COL);
+//	//DisplayBoard(mine, ROW, COL);
+//	
+//	//排查雷
+//	FindMine(mine,show,ROW,COL);
+//
+//
+//}
+//
+//int main()
+//{
+//	int input = 0;
+//	srand((unsigned int)time(NULL));
+//	do 
+//	{
+//		menu();
+//		printf("请选择：");
+//		scanf("%d", &input);
+//		switch(input)
+//		{
+//		case 1:
+//			printf("游戏开始！\n");
+//			game();//扫雷游戏
+//			break;
+//		case 0:
+//			printf("退出游戏\n");
+//			break;
+//		default:
+//			printf("输入无效，请重新输入\n");
+//			break;
+//		}
+//	} while (input);
+//	
+//	return 0;
+//}
+
 #include"game.h"
 
 void menu()
 {
-	printf("1.play\n");
-	printf("0.exit\n");
+	printf("*****************************************\n");
+	printf("*********  1.play      0.exit   *********\n");
+	printf("*****************************************\n");
 }
 
 void game()
 {
-	char mine[ROWS][COLS] = { 0 };//存放布置好的雷的信息
-	char show[ROWS][COLS] = { 0 };//存放排查出的雷的信息
-	
+	//存放地雷的棋盘
+	char mine[ROWS][COLS] = { 0 };
+	//展示给用户看的棋盘
+	char show[ROWS][COLS] = { 0 };
 	//初始化棋盘
-	InitBoard(mine, ROWS, COLS, '0');
-	InitBoard(show, ROWS, COLS, '*');
-	
+	init_board(mine, ROWS, COLS, '0');
+	init_board(show, ROWS, COLS, '*');
+	//放置地雷
+	set_mine(mine, ROW, COL);
+	system("cls");//清空屏幕
 	//打印棋盘
-	DisplayBoard(show, ROW, COL);
-	
-	//布置雷
-	SetMine(mine, ROW, COL);
-	//DisplayBoard(mine, ROW, COL);
-	
-	//排查雷
-	FindMine(mine,show,ROW,COL);
-
-
+	//display_board(mine, ROW, COL);
+	display_board(show, ROW, COL);
+	//开始扫雷
+	find_mind(mine, show, ROW, COL);
 }
 
 int main()
 {
-	int input = 0;
+	//设置随机数的起点
 	srand((unsigned int)time(NULL));
-	do 
+	int input = 0;
+	do
 	{
-		menu();
+		menu();//菜单
 		printf("请选择：");
 		scanf("%d", &input);
-		switch(input)
+		switch (input)
 		{
 		case 1:
-			printf("游戏开始！\n");
-			game();//扫雷游戏
+			game();
 			break;
 		case 0:
 			printf("退出游戏\n");
 			break;
 		default:
-			printf("输入无效，请重新输入\n");
+			printf("输入错误，请重新输入\n");
 			break;
 		}
 	} while (input);
-	
 	return 0;
 }
